69d470c2476bac96308af065cbe16846b8c3e54d
Hudson Murray <hudsonjmurray11@gmail.com>
2026-05-24 17:14:15 -0400

    Fix checkout.js: use module.exports instead of export default for CommonJS compatibility

--- api/checkout.js
+++ api/checkout.js
@@ -1,6 +1,6 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
     if (req.method !== 'POST') {
         return res.status(405).json({ error: 'Method not allowed' });
     }
